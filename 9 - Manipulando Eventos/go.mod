9e192eb2abefd73a5de45dfad2330811a6a2b368
leoneville <leoneville_@hotmail.com>
2026-06-21 15:27:05 -0300

    feat: define interfaces de eventos e implementa o metodo Register no EventDispatcher

---
+++ 9 - Manipulando Eventos/go.mod
@@ -0,0 +1,3 @@
+module github.com/leoneville/fcutils
+
+go 1.26.1
---
+++ 9 - Manipulando Eventos/pkg/events/event_dispatcher.go
@@ -0,0 +1,29 @@
+package events
+
+import (
+	"errors"
+	"slices"
+)
+
+var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		if slices.Contains(ed.handlers[eventName], handler) {
+			return ErrHandlerAlreadyRegistered
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
