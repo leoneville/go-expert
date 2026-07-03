d40358ebb9fe40e0a03422ab29422bee66b39e36
Neville Guimarães <nevilleguimaraes@gmail.com>
2026-07-03 16:02:51 -0300

    feat: implementa CreateCategoryStreamBidirectional no servico de categoria do gRPC

--- 12-gRPC/internal/service/category.go
+++ 12-gRPC/internal/service/category.go
@@ -90,3 +90,30 @@ func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateC
 		})
 	}
 }
+
+func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+	ctx := stream.Context()
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		categoryResult, err := c.CategoryDB.Create(ctx, category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		err = stream.Send(&pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+		if err != nil {
+			return err
+		}
+	}
+}
